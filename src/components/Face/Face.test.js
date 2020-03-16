2baf08fc4f49ff3915bff8ef1b45c850950e394c
Charlie Richardson <Charlie.Richardson@Nology.local>
2020-03-16 21:06:14 +0000

    added tests

--- src/components/Face/Face.test.js
+++ src/components/Face/Face.test.js
@@ -1,10 +1,51 @@
 import React from "react";
 import Face from "./Face";
+import people from "../../data/people";
+import { mount } from "enzyme";
+import Eyes from "../../components/Eyes";
 
 describe("Face tests", () => {
   let component;
+  const mockPerson = people[0];
 
   beforeEach(() => {
-    component =
-  })
+    component = mount(<Eyes isLeft={true} leftEye={mockPerson.leftEyeImg} />);
+  });
+
+  it("should render", () => {
+    expect(component).toBeTruthy();
+  });
+
+  it("should match left image url", () => {
+    expect(component.find("img").prop("src")).toBe(mockPerson.leftEyeImg);
+  });
+
+  it("should match right image url", () => {
+    component = mount(<Eyes leftEye={mockPerson.rightEyeImg} />);
+    expect(component.find("img").prop("src")).toBe(mockPerson.rightEyeImg);
+  });
+});
+
+describe("HallOfMirrors tests", () => {
+  const mockPerson = people[0];
+  let component;
+
+  beforeEach(() => {
+    component = mount(
+      <Face
+        name={mockPerson.name}
+        face={mockPerson.faceImg}
+        leftEye={mockPerson.leftEyeImg}
+        rightEye={mockPerson.rightEyeImg}
+      />
+    );
+  });
+
+  it("should render", () => {
+    expect(component).toBeTruthy();
+  });
+
+  it("should render one face component", () => {
+    expect(component.find(Face).length).toEqual(1);
+  });
 });
--- src/containers/HallOfMirrors/HallOfMirrors.test.js
+++ src/containers/HallOfMirrors/HallOfMirrors.test.js
@@ -1,10 +1,31 @@
 import React from "react";
 import HallOfMirrors from "./HallOfMirrors";
+import people from "../../data/people";
+import { mount } from "enzyme";
+import Face from "../../components/Face"
 
 describe("HallOfMirrors tests", () => {
+  const mockPerson = people[0];
   let component;
 
   beforeEach(() => {
-    component =
-  })
+    component = mount(
+      <Face
+        name={mockPerson.name}
+        face={mockPerson.faceImg}
+        leftEye={mockPerson.leftEyeImg}
+        rightEye={mockPerson.rightEyeImg}
+      />
+    );
+  });
+
+  it("should render", () => {
+    expect(component).toBeTruthy();
+  });
+
+  it("should render one face component", () => {
+    expect(component.find(Face).length).toEqual(1);
+  });
+
+  
 });
