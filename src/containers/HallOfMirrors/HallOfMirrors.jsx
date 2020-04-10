f27b23d6623c2079e4987f236c91107415cc60d7
Charlie Richardson <Charlie.Richardson@Nology.local>
2020-04-10 14:06:51 +0100

    display 1 center etc

--- src/containers/HallOfMirrors/HallOfMirrors.jsx
+++ src/containers/HallOfMirrors/HallOfMirrors.jsx
@@ -5,22 +5,27 @@ import people from "../../data/people";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faInfoCircle } from "@fortawesome/free-solid-svg-icons";
 
-const HallOfMirrors = props => {
+const HallOfMirrors = (props) => {
   const { handleClick, numMirrors } = props;
+
+  const checkMirrorNum = (number) =>
+    number === 1 ? "oneMirror" : number === 2 ? "twoMirror" : "";
+
+  const mirrorDisplay = numMirrors ? checkMirrorNum(numMirrors) : "";
 
   return (
     <>
-      <section className={styles.mirror}>
+      <section className={`${styles.mirror} ${styles[mirrorDisplay]}`}>
         <FontAwesomeIcon
           icon={faInfoCircle}
           className={styles.info}
-          onClick={event => handleClick(event.target.value)}
+          onClick={(event) => handleClick(event.target.value)}
         />
         <div className={styles.numInfo}>
           <p>The random number API has given you {numMirrors} to view.</p>
           <p>{numMirrors}</p>
         </div>
-        {people.slice(0, numMirrors).map(person => (
+        {people.slice(0, numMirrors).map((person) => (
           <Face
             name={person.name}
             face={person.faceImg}
